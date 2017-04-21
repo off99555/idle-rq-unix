517ab6344405a44ae1be59f621b43bf11fc5c3f4
Chanchana Sornsoontorn <off9955555@gmail.com>
2017-04-21 13:08:43 +0700

    Fix infinite resend bug

--- idle-rq.c
+++ idle-rq.c
@@ -4,10 +4,13 @@
 #include "idle-rq.h"
 #include "trouble-maker.h"
 
+#define BUFSIZE 10000
+
 int N = 0; // current sequence number
 
 char* makeframes(char *buf, size_t len);
 int parity(char frame);
+int corrupted(char frame);
 
 ssize_t mysend(int sockfile, const void *buf, size_t len, int flags) {
   char *tmp = (char*)buf;
@@ -25,9 +28,33 @@ ssize_t mysend(int sockfile, const void *buf, size_t len, int flags) {
   size_t n = strlen(frames);
   printf("Frames (%zu):\n", n);
   int i;
-  for (i = 0; i < 999999; i++) {
+  for (i = 0; i < n; i++) {
+    printf("Sending frame %d: ", i);
     printbits(frames[i]);
-    mightsend(sockfile, 'x');
+    mightsend(sockfile, frames[i]);
+    // TODO: set a timer
+    // wait for S to respond
+    char ack; // we prefer first bit to say ACK, if it's 1 or NAK if 0
+    recv(sockfile, &ack, 1, 0);
+    printf("Receiving: ");
+    printbits(ack);
+    int isack = ack & 1;
+    printf("It's %s\n", isack ? "ACK" : "NAK");
+    int wanted = N == ((ack >> 6) & 1);
+    printf("Its order is %s\n", wanted ? "valid" : "NOT valid");
+    if (!wanted) {
+      printf("Expected N=%d, got %d\n", N, ((ack >> 6) & 1));
+    }
+    int corrup = corrupted(ack);
+    if (isack // if the 1st bit is ack
+        && wanted // if seqNo is valid
+        && !corrup) { // if isn't corrupted
+      printf("Go send next frame.\n");
+      N = !N;
+    } else {
+      printf("Resend this frame again.\n");
+      i--;
+    }
   }
   return len;
 }
@@ -36,9 +63,52 @@ ssize_t myrecv(int sockfile, void *buf, size_t len, int flags) {
   // forever try to receive frames
   // for each frame, if corrupted or not proper order send NAK frame,
   // else send ACK and if last frame break
+  char frames[BUFSIZE];
+  char ack;
+  int i = 0;
+  while (1) {
+    recv(sockfile, frames+i, 1, 0);
+    printf("Receiving: ");
+    printbits(frames[i]);
+    int corrup = corrupted(frames[i]);
+    if (corrup) {
+      fprintf(stderr, "The frame is corrupted.\n");
+    }
+    int wanted = N == ((frames[i] >> 6) & 1);
+    if (!wanted) {
+      fprintf(stderr, "The frame is not what we wanted.\n");
+      printf("Expected N=%d, got %d\n", N, ((frames[i] >> 6) & 1));
+    }
+    if (corrup || !wanted) {
+      ack = 0;
+    } else {
+      ack = 1;
+    }
+
+    // add seqNo bit
+    if (N) { // if wanting odd seqNo
+      ack |= 1 << 6; // turn on the seqNo bit
+    }
+
+    // add parity bit
+    if (parity(ack)) {
+      ack |= 1 << 7;
+    }
+    printf("Sending: ");
+    printbits(ack);
+    mightsend(sockfile, ack);
+    if (!corrup && wanted)
+      N = !N;
+
+    // check for last frame
+    if ((ack >> 5) & 1) {
+      break;
+    }
+    i++;
+  }
 
   // join packets from frames together into *buf
-  return recv(sockfile, buf, len, flags);
+  return len;
 }
 
 // split data into packets then make frames containing them
@@ -90,4 +160,8 @@ int parity(char frame) {
     result ^= (frame >> i) & 1;
   }
   return result;
+}
+
+int corrupted(char frame) {
+  return parity(frame) != ((frame >> 7) & 1);
 }
