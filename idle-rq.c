e6b6f76f9e2c889519579fe6c09eccb59272496a
Chanchana Sornsoontorn <off9955555@gmail.com>
2017-04-27 02:12:51 +0700

    Correctly implement idle-rq from the book

--- idle-rq.c
+++ idle-rq.c
@@ -4,7 +4,6 @@
 #include "idle-rq.h"
 #include "trouble-maker.h"
 
-int N = 0; // current sequence number
 const int PARITY_BIT = 15;
 const int SEQ_BIT = 14;
 const int LAST_INDICATOR_BIT = 13;
@@ -33,14 +32,18 @@ ssize_t mysend(int sockfile, const void *buf, size_t len, int flags) {
   printf("Frames (%zu):\n", n);
   int i;
   for (i = 0; i < n; i++) {
+    // IDLE state: assume that we receive an incoming I-frame
+    // from above layer without the need to wait then we send it immediately
     printf("Sending I-frame %d: ", i);
     printbits(frames[i]);
     printstat(frames[i]);
+    // TxFrame (format and transmit the frame)
     mightsend(sockfile, frames[i]);
-    // TODO: set a timer
-    // WTACK state: wait for Secondary to respond
+    // TODO: Start_timer; Vs++;
+    // PresentState = WTACK: wait for Secondary to respond
     short ack; // we prefer ACK_BIT bit to mean ACK, if it's 1 or NAK if 0
     ssize_t status = recv(sockfile, &ack, 2, 0); // receiving the ACK frame
+    // TODO: TEXP: if time expire do RetxFrame; Start_timer; PresentState=WTACK;
     if (status == 0) {
       // the Secondary has closed the socket
       printf("Secondary has closed connection, indicating proper transmission. ACK frame not needed. Primary process is terminating.\n");
@@ -51,19 +54,42 @@ ssize_t mysend(int sockfile, const void *buf, size_t len, int flags) {
     printf("Receiving %s frame: ", corrup ? "a corrupted" : isack ? "ACK" : "NAK");
     printbits(ack);
     printstat(ack);
-    int wanted = N == testbit(ack, SEQ_BIT);
-    if (!corrup && !wanted) { // show msg only when the frame is not corrupted
-      printf("Its order is %s\n", wanted ? "valid" : "NOT valid");
-      printf("Expected N=%d, got %d\n", N, testbit(ack, SEQ_BIT));
-    }
-    if (isack // if the 1st bit is ack
-        && wanted // if seqNo is valid
-        && !corrup) { // if isn't corrupted
-      /* printf("Go send next frame.\n"); */
-      N = !N;
+    if (isack) {
+      int NS = testbit(frames[i], SEQ_BIT);
+      int NR = testbit(ack, SEQ_BIT);
+      int P0 = NS == NR;
+      int P1 = !corrup;
+      if (P0) {
+        printf("Its order is %s\n", P0 ? "valid" : "NOT valid");
+        printf("Expected N=%d, got %d\n", NS, NR);
+        if (P1) {
+          // TODO: begin Stop_timer;
+          // State=IDLE (in our implementation, go send another frame immediately)
+          continue;
+        } else {
+          // RetxFrame; retransmit I-frame waiting acknowledgement
+          printf("Resend this I-frame again.\n");
+          // Start_timer;
+          // PresentState = WTACK
+          // all of the above can be accomplished by redoing this loop
+          i--;
+          continue;
+        }
+      } else {
+        if (!P1) {
+          // PresentState = IDLE;
+          // Error++;
+          printf("Error: Wrong Seq and corrupted\n");
+        } else {
+          // do nothing
+          printf("Wrong Seq and not corrupted, should be impossible to get here\n");
+        }
+      }
     } else {
+      // RetxFrame; Start_timer; PresentState=WTACK;
       printf("Resend this I-frame again.\n");
       i--;
+      continue;
     }
   }
   return len;
@@ -74,49 +100,60 @@ ssize_t myrecv(int sockfile, void *buf, size_t len, int flags) {
   // for each frame, if corrupted or not proper order send NAK frame,
   // else send ACK and if last frame break
   short frames[len+1], frame;
-  short ack = 0;
-  int i = 0;
+  short ack; // declared for temporary uses
+  int i = 0; // frame number that we are waiting for
   while (1) {
+    // PresentState=WTIFM; Waiting for event: IRCVD
     ssize_t status = recv(sockfile, &frame, 2, 0);
     if (status == 0) {
       fprintf(stderr, "Primary has closed connection, unexpected behavior!\n");
     }
     int corrup = corrupted(frame);
+    int NS = testbit(frame, SEQ_BIT);
+    int Vr = i % 2;
+    int P0 = NS == Vr;
+    int P1 = !corrup;
+    int P2 = NS == !Vr;
     int last = testbit(frame, LAST_INDICATOR_BIT);
-    int wanted = N == testbit(frame, SEQ_BIT);
-    if (!corrup && !wanted) {
-      i--;
-    }
     printf("Receiving %sI-frame %d: ", corrup ? "a corrupted " : last ? "the last " : "", i);
     printbits(frame);
     printstat(frame);
-    if (!corrup && !wanted) {
-      fprintf(stderr, "The I-frame order is invalid. Duplicate detected.\n");
-      printf("Expected N=%d, got %d\n", N, testbit(frame, SEQ_BIT));
+    ack = 0;
+    int X = NS;
+    int isack;
+    if (!P1) {
+      // TxNAK(X);
+      isack = 0;
+    } else {
+      if (P2) {
+        // TxACK(X);
+        isack = 1;
+        fprintf(stderr, "The I-frame order is invalid. Duplicate detected.\n");
+        printf("Expected N=%d, got %d\n", Vr, X);
+      } else if (P0) {
+        // LDATAind: Pass contents of received I-frame to user AP with
+        // L_DATA.indication primitive
+        frames[i] = frame;
+        // TxACK(X);
+        isack = 1;
+        // Vr = Vr + 1;
+        i++;
+      } else {
+        printf("P1 and not P2 and not P0, impossible\n");
+      }
     }
-    setbit(&ack, ACK_BIT, !corrup);
-
-    // add seqNo bit
-    if (wanted && N || (!wanted && testbit(frame, SEQ_BIT))) { // if wanting odd seqNo
-      setbit(&ack, SEQ_BIT, 1);
-    }
-
-    // add parity bit
+    setbit(&ack, ACK_BIT, isack);
+    setbit(&ack, SEQ_BIT, X);
     setbit(&ack, PARITY_BIT, parity(ack));
     printf("Sending %s frame: ", testbit(ack, ACK_BIT) ? "ACK" : "NAK");
     printbits(ack);
     printstat(ack);
     mightsend(sockfile, ack);
 
-    if (testbit(ack, ACK_BIT)) {
-      N = !N;
-      frames[i] = frame;
-      i++;
-    }
     // check for last frame
     if (last) {
       /* printf("This is the last I-frame.\n"); */
-      //TODO: try to not send the last ACK frame back and let Primary notice
+      // you can try to not send the last ACK frame back and let Primary notice
       // that you got the last I-frame by closing the socket
       // Primary should check if the socket is closed that mean you are done
       // and it should stop resending the last I-frame to you and stop waiting
@@ -158,16 +195,17 @@ short *makeframes(char *buf, size_t len) {
       if (buf[fNo] & (1 << i)) {
         frames[fNo] |= 1 << i;
       }
-      if (buf[fNo] == 0 || fNo >= len) { // if run out of buf
-        setbit(frames+fNo, LAST_INDICATOR_BIT, 1); // last frame indicator
-        done = 1;
-        break; // go add header and leave unused bits blank
-      }
     }
 
     // add seqNo bit
     if (fNo % 2) { // if current frame sequence number is odd
       setbit(frames+fNo, SEQ_BIT, 1); // turn on the seqNo bit
+    }
+
+    if (buf[fNo+1] == 0 || fNo+1 >= len) { // if buf[fNo] is the last element
+        setbit(frames+fNo, LAST_INDICATOR_BIT, 1); // last frame indicator
+        done = 1;
+        frames[fNo+1] = 0;
     }
 
     // add parity bit
@@ -194,8 +232,8 @@ int corrupted(short frame) {
 }
 
 void printstat(short frame) {
-  printf("ACK: %d\n", testbit(frame, ACK_BIT));
-  printf("Last: %d\n", testbit(frame, LAST_INDICATOR_BIT));
-  printf("SEQ: %d\n", testbit(frame, SEQ_BIT));
+  printf("ACK: %d ", testbit(frame, ACK_BIT));
+  printf("Last: %d ", testbit(frame, LAST_INDICATOR_BIT));
+  printf("SEQ: %d ", testbit(frame, SEQ_BIT));
   printf("Parity: %d\n", testbit(frame, PARITY_BIT));
 }
